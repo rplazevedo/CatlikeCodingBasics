ffb6f8dbb11167d1327bee2ba9a14fde1412371d
D. Azevedo <rplazevedo@gmail.com>
2023-11-26 21:59:12 +0000

    Set up update function

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Graph : MonoBehaviour
 {
@@ -9,21 +10,35 @@ public class Graph : MonoBehaviour
     [SerializeField, Range(10,100)]
     int resolution = 10;
 
+    Transform[] points;
+
+
     private void Awake()
     {
-        Console.Write("Awake");
         float step = 2f / resolution;
         var position = Vector3.zero;
         var scale = Vector3.one * step;
-        for (int i = 0; i < resolution; i++)
+
+        points = new Transform[resolution];
+
+        for (int i = 0; i < points.Length; i++)
         {
-            Console.Write(i);
-            Transform point = Instantiate(pointPrefab);
+            Transform point = points[i] = Instantiate(pointPrefab);
             position.x = (i + 0.5f) * step - 1f;
             position.y = position.x * position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
+        }
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = position.x * position.x * position.x;
         }
     }
 }
