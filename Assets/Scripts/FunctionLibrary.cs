c0ab1aa5682788c548a0ac26bc5db518a4a88260
D. Azevedo <rplazevedo@gmail.com>
2023-12-03 14:33:41 +0000

    Functions in array now

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -1,25 +1,13 @@
-using UnityEngine;
-using static FunctionLibrary;
-using UnityEngine.UIElements;
 using static UnityEngine.Mathf;
 public static class FunctionLibrary
 {
     public delegate float Function(float x, float t);
 
+    static Function[] functions = {Wave, MultiWave, Ripple};
+
     public static Function GetFunction (int index)
     {
-        if (index == 0)
-        {
-            return Wave;
-        }
-        else if (index == 1)
-        {
-            return MultiWave;
-        }
-        else
-        {
-            return Ripple;
-        }
+        return functions[index];
     }
     public static float Wave (float x, float t)
     {
