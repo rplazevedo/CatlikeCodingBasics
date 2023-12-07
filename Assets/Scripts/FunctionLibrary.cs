520dbddcb9a90e5ee861f3559c9ed5b99210a15e
D. Azevedo <rplazevedo@gmail.com>
2023-12-07 14:05:32 +0000

    Added Trippy torus. Finished Mathematical Surfaces.

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -5,9 +5,9 @@ public static class FunctionLibrary
 {
     public delegate Vector3 Function(float u, float v, float t);
 
-    static Function[] functions = { Wave, MultiWave, Ripple , Sphere};
+    static Function[] functions = { Wave, MultiWave, Ripple, Sphere, Torus };
 
-    public enum FunctionName { Wave, MultiWave, Ripple, Sphere};
+    public enum FunctionName { Wave, MultiWave, Ripple, Sphere, Torus };
 
     public static Function GetFunction (FunctionName name)
     {
@@ -53,6 +53,18 @@ public static class FunctionLibrary
         p.x = s * Sin(PI * u + t);
         p.y = r * Sin(0.5f * PI * v);
         p.z = s * Cos(PI * u + t);
+        return p;
+    }
+
+    public static Vector3 Torus(float u, float v, float t)
+    {
+        float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+        float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+        float s = r1 + r2 * Cos(PI * v);
+        Vector3 p;
+        p.x = s * Sin(PI * u );
+        p.y = r2 * Sin(PI * v);
+        p.z = s * Cos(PI * u );
         return p;
     }
 }
