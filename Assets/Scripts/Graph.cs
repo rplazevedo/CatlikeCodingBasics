d5c976498428659e89b4b2d25aa69f5307b90aea
D. Azevedo <rplazevedo@gmail.com>
2023-12-19 15:18:51 +0000

    Added function morphing. Measuring Performance tutorial completed!

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -13,10 +13,14 @@ public class Graph : MonoBehaviour
     [SerializeField] TransitionMode transitionMode;
 
     [SerializeField, Min(0f)] float functionDuration = 1f;
+    [SerializeField, Min(0f)] float transitionDuration = 1f;
 
     Transform[] points;
 
     float duration;
+    bool transitioning;
+
+    FunctionLibrary.FunctionName transitionFunction;
 
     private void Awake()
     {
@@ -36,12 +40,29 @@ public class Graph : MonoBehaviour
     private void Update()
     {
         duration += Time.deltaTime;
-        if (duration >= functionDuration)
+        if ( transitioning)
+        {
+            if (duration>= transitionDuration)
+            {
+                duration -= transitionDuration;
+                transitioning = false;
+            }
+        }
+        else if (duration >= functionDuration)
         {
             duration -= functionDuration;
+            transitioning = true;
+            transitionFunction = function;
             PickNextFunction();
         }
-        UpdateFunction();
+        if (transitioning)
+        {
+            UpdateFunctionTransition();
+        }
+        else
+        {
+            UpdateFunction();
+        }
     }
 
 
@@ -69,6 +90,29 @@ public class Graph : MonoBehaviour
             float u = (x + 0.5f) * step - 1f;
 
             points[i].localPosition = f(u, v, time);
+        }
+    }
+
+    private void UpdateFunctionTransition()
+    {
+        FunctionLibrary.Function from = FunctionLibrary.GetFunction(transitionFunction);
+        FunctionLibrary.Function to = FunctionLibrary.GetFunction(function);
+
+        float progress = duration / transitionDuration;
+        float time = Time.time;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        {
+            if (x == resolution)
+            {
+                x = 0;
+                z++;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+
+            points[i].localPosition = FunctionLibrary.Morph(u, v, time, from, to, progress);
         }
     }
 }
