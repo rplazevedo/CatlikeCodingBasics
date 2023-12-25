fe1642110b826375b0e576f07c7d6f967e43d6c9
D. Azevedo <rplazevedo@gmail.com>
2023-12-25 17:34:16 +0000

    Setting up Compute Shaders

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -4,7 +4,7 @@ public class Graph : MonoBehaviour
 {
     [SerializeField] Transform pointPrefab;
 
-    [SerializeField, Range(10,100)] int resolution = 10;
+    [SerializeField, Range(10,200)] int resolution = 10;
 
     [SerializeField] FunctionLibrary.FunctionName function;
 
