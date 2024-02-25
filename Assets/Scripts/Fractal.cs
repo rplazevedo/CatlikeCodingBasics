54fb45363eae411366348e0488cc9e65808b399c
D. Azevedo <rplazevedo@gmail.com>
2024-02-25 15:50:08 +0000

    Started Jobs tutorial

---
+++ Assets/Scripts/Fractal.cs
@@ -0,0 +1,33 @@
+
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class Fractal : MonoBehaviour
+{
+    [SerializeField, Range(1, 8)] int depth = 4;
+
+    private void Start()
+    {
+        name = "Fractal " + depth;
+
+        if ( depth <= 1)
+        {
+            return;
+        }
+
+        Fractal childA = CreateChild(Vector3.right);
+        Fractal childB = CreateChild(Vector3.up);
+
+        childA.transform.SetParent(transform, false);
+        childB.transform.SetParent(transform, false);
+    }
+
+    Fractal CreateChild(Vector3 direction)
+    {
+        Fractal child = Instantiate(this);
+        child.depth = depth - 1;
+        child.transform.localPosition = 0.75f * direction;
+        child.transform.localScale = 0.5f * Vector3.one;
+        return child;
+    }
+}
