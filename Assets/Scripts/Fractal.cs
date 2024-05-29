b810373445bb1ede64fa3679c98a3b9830cb162b
D. Azevedo <rplazevedo@gmail.com>
2024-05-29 21:30:25 +0100

    Added TRS matrices

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -31,13 +31,16 @@ public class Fractal : MonoBehaviour
     }
 
     FractalPart[][] parts;
+    Matrix4x4[][] matrices;
 
     private void Awake()
     {
         parts = new FractalPart[depth][];
+        matrices = new Matrix4x4[depth][];
         for (int i = 0, length = 1; i < parts.Length; i++, length *= 5) 
         {
             parts[i] = new FractalPart[length];
+            matrices[i] = new Matrix4x4[length];
         }
 
         parts[0][0] = CreatePart(0);
@@ -47,7 +50,7 @@ public class Fractal : MonoBehaviour
             for (int fpi = 0; fpi < levelParts.Length; fpi += 5)
                 for (int ci = 0; ci < 5; ci++)
                 {  
-                    levelParts[fpi + ci] = CreatePart(ci;
+                    levelParts[fpi + ci] = CreatePart(ci);
                 }
         }
     }
@@ -67,6 +70,7 @@ public class Fractal : MonoBehaviour
         rootPart.rotation *= deltaRotation;
         rootPart.worldRotation = rootPart.rotation;
         parts[0][0] = rootPart;
+        matrices[0][0] = Matrix4x4.TRS(rootPart.worldPosition, rootPart.worldRotation, Vector3.one);
 
         float scale = 1f;
         for (int li = 1; li < parts.Length; li++)
@@ -74,6 +78,7 @@ public class Fractal : MonoBehaviour
             scale *= 0.5f;
             FractalPart[] parentParts = parts[li - 1];
             FractalPart[] levelParts = parts[li];
+            Matrix4x4[] levelMatrices = matrices[li];
             for (int fpi = 0; fpi < levelParts.Length; fpi++)
             {
                 FractalPart parent = levelParts[fpi / 5];
@@ -86,6 +91,7 @@ public class Fractal : MonoBehaviour
                     parent.worldRotation *
                     (1.5f *scale * part.direction);
                 levelParts[fpi] = part;
+                levelMatrices[fpi] = Matrix4x4.TRS(part.worldPosition, part.worldRotation, scale * Vector3.one);
             }
         }
     }
